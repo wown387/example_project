0459a435e4b57c0791a5c7cca156d34045b38944
이우주 <wown387@naver.com>
2025-01-28 23:51:35 +0900

    init

---
+++ example_app/views.py
@@ -0,0 +1,10 @@
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def hello_rest_api(request):
+    data = {'message': 'Hello, REST API!'}
+    return Response(data)
